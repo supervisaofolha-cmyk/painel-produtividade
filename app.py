08f273e348f5462b0349a097129b5070dbe4eb75
supervisaofolha-cmyk <supervisaofolha@gmail.com>
2026-06-03 08:36:48 -0300

    auto-sync 2026-06-03 08:36:44

--- app.py
+++ app.py
@@ -86,6 +86,9 @@ SGD_RELATORIO_URL = f"{SGD_BASE_URL}/sgsc/faces/rel-satisfacao.html"
 SGD_RELATORIO_TIMEOUT = 300
 SGD_RELATORIO_TENTATIVAS = 3
 RO_DRIVE_FOLDER_URL = "https://drive.google.com/drive/folders/1O-1uJ6D9al9piHgOv_Ju0fpL-3Xbz3zK"
+RO_PLANILHAS_FIXAS = {
+    (2026, 6): "https://docs.google.com/spreadsheets/d/1AZKhjWnIS_hys9B9oaeazcSUP0wS6ExX8U9wRdrktRs/edit?resourcekey#gid=148357412",
+}
 PONTOWEB_BASE_URL = "https://pontoweb.secullum.com.br/"
 PONTOWEB_CLIENT_ID = "3001"
 PONTOWEB_REDIRECT_URI = f"{PONTOWEB_BASE_URL}Auth"
